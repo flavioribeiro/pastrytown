7baa64bf40eed73f857bf2a6c639b69410661704
Flavio Ribeiro <email@flavioribeiro.com>
2021-11-12 08:38:58 -0800

    add initial rating system

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,19 +1,26 @@
 import React, { useState } from "react";
+import ReactStars from "react-rating-stars-component";
 import "../styles.css";
 
 const TodoItem = (props) => {
   const [isComplete, setIsComplete] = useState(props.item.completeStatus);
 
-  const handleChange = () => {
+  const handleCompleteChange = () => {
     props.item.completeStatus = !props.item.completeStatus;
     setIsComplete(props.item.completeStatus);
   };
+
+  const handleRatingChange = (newRating) => {
+    console.log(newRating);
+  }
+
   return (
     <div className="todo-item">
-      <input type="checkbox" checked={isComplete} onChange={handleChange} />
+      <input type="checkbox" checked={isComplete} onChange={handleCompleteChange} />
       <p className={isComplete ? "completed-style" : null}>
         {props.item.description}
       </p>
+      <ReactStars count={5} onChange={handleRatingChange} size={24} activeColor="#ffd700" />
     </div>
   );
 };
